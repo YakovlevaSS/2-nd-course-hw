d9f471ed2fdf3aec26fa6889f273c700776f3f86
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-04-25 22:56:16 +0300

    add files hw_3

---
+++ hw_3.js
@@ -0,0 +1,28 @@
+//task 1
+let password = 'password';
+let userPassword = prompt ('Введите пароль');
+if (password ===userPassword) {
+    console.log('Пароль введён верно');    
+} else {
+    console.log('Пароль введён неправильно');    
+}
+//task 2
+let c = -3;
+if (c>0 && c<10) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+//task 3
+let d = 400;
+let e = 34;
+if (d>100 || e>100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+//task 4
+let a = '2';
+let b = '3';
+alert (Number(a) + Number(b));
+
