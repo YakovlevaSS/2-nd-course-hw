028b8ace188ac72b452c4206e2eb1ce29bf15cd5
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-04-27 12:54:26 +0300

    Correct files hw_3.js, index.html, style.scss

--- hw_3.js
+++ hw_3.js
@@ -1,14 +1,14 @@
 //task 1
 let password = 'password';
-let userPassword = prompt ('Введите пароль');
-if (password ===userPassword) {
-    console.log('Пароль введён верно');    
+let userPassword = prompt('Введите пароль');
+if (password === userPassword) {
+    console.log('Пароль введён верно');
 } else {
-    console.log('Пароль введён неправильно');    
+    console.log('Пароль введён неправильно');
 }
 //task 2
 let c = -3;
-if (c>0 && c<10) {
+if (c > 0 && c < 10) {
     console.log('Верно');
 } else {
     console.log('Неверно');
@@ -16,7 +16,7 @@ if (c>0 && c<10) {
 //task 3
 let d = 400;
 let e = 34;
-if (d>100 || e>100) {
+if (d > 100 || e > 100) {
     console.log('Верно');
 } else {
     console.log('Неверно');
@@ -24,5 +24,63 @@ if (d>100 || e>100) {
 //task 4
 let a = '2';
 let b = '3';
-alert (Number(a) + Number(b));
+alert(Number(a) + Number(b));
+//task 5
+let month = prompt('Введите номер месяца');
+switch (true) {
+    case (month === '1' || month === '2' || month === '12'):
+        console.log('Зимний месяц');
+        break;
+    case (month === '3' || month === '4' || month === '5'):
+        console.log('Весенний месяц');
+        break;
+    case (month === '6' || month === '7' || month === '8'):
+        console.log('Летний месяц');
+        break;
+    case (month === '9' || month === '10' || month === '11'):
+          console.log('Осенний месяц');
+        break;
+   case (month >= 13 || month <= 0):
+        console.log('Такого месяца не существует');
+        break;
+    default:
+        console.log('Вы ввели запрос с ошибкой');
+        break;
+} 
 
+//task 7
+let Number = prompt ('Введите число');
+if (isNaN(Number) === true) {
+    alert('Вы ввели не числовое значение');
+} else {
+    let result = Number % 2;
+}
+if (result = 0) {
+    alert ('Число является чётным');
+} else {
+    alert ('Число является нечётным');
+} //Возможно решение, когда до ветвления присвоить переменногй "result" значение, равное остатку от деления на 2. А проверку осуществить через несколько условий (else if).
+
+//task 8
+let clienOS = 1; //Переменная определяет ОС мобильного устройства, с которого клиент посетил сайт 0 - IOS, 1 - Android
+let clientDevice = 1; //Переменная определяет с которого устройства клиент посетил сайт 0 - ПК, 1 - мобильный телефон
+if (clientDevice === 1) {
+    if (clienOS === 0 ) {
+        alert ('Установите версию приложения для IOS по ссылке');
+    } else {
+        alert ('Установите версию приложения для Android по ссылке');
+    }
+}
+//task 9
+let year = 2015;
+if (clientDevice === 1) {
+    if (clienOS === 0 && year < 2015) {
+        alert ('Установите облегчённую версию приложения для IOS по ссылке');
+    } else if (clienOS === 1 && year < 2015) {
+        alert ('Установите облегчённую версию приложения для Android по ссылке');
+    } else if (clienOS === 0 && year >= 2015) {
+        alert ('Установите версию приложения для IOS по ссылке');
+    } else {
+        alert ('Установите версию приложения для Android по ссылке');
+}
+}
