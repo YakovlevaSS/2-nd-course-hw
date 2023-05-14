9dac03836a0e0a0afe545c67b6a3105869a9455a
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-05-14 23:34:42 +0300

    Done tasks from 1 to 8

---
+++ hw_6.js
@@ -0,0 +1,45 @@
+// task 1
+const numbs = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < numbs.length; i++) {
+    console.log(numbs[i]);
+    if (numbs[i] === 10) {
+        break;
+    }
+}
+//task 2
+console.log(numbs.indexOf(4) + 1); // из условия не понятно, если именно поожение эдемента найти, то 3. Если нужно было просто вывести индекс элемента "4", то убрать "+1" из формулы
+
+//task 3
+const arr = [1, 3, 5, 10, 20];
+let str = arr.join(' ');
+console.log(str);
+
+//task 4
+
+//task 5
+const arrOne = [1, 1, 1];
+const newArrOne = arrOne.concat(2, 2, 2);
+console.log(newArrOne);
+
+//Task 6
+const arrSort = [9, 8, 7, 'a', 6, 5];
+arrSort.sort();
+arrSort.pop();
+console.log(arrSort);
+
+//Task 7
+const arrCheck = [9, 8, 7, 6, 5];
+let check = +prompt('Введите число');
+if (arrCheck.includes(check)) {
+    console.log(`Элемент ${check} содержится в архиве`);
+}
+else {
+    console.log(`Элемент ${check} не содержится в архиве`);
+}
+
+//Task 8
+str = 'abcdef';
+const strResult = str.split('').reverse().join('')
+console.log(strResult);
+
+//Task 9
